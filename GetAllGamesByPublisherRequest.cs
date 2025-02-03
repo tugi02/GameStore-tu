98d0615bb4b368d792d2fc1b70da3192d3b2cf3c
tugi02 <stu2113811063@uni-plovdiv.bg>
2025-02-03 15:30:53 +0200

    hcheck

---
+++ GetAllGamesByPublisherRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace GameStore.Models.Requests
+{
+    public class GetAllGamesByPublisherRequest
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public int PublisherId { get; set; }
+
+        public DateTime AfterDate { get; set; }
+    }
+}
