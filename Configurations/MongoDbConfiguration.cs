2d6be89ea4f2b9f2777f31ff51f834b483d41627
tugi02 <stu2113811063@uni-plovdiv.bg>
2025-02-03 18:35:17 +0200

    123

--- Configurations/MongoDbConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GameStore.Models.Configurations
-{
-    public class MongoDbConfiguration
-    {
-        public string ConnectionString { get; set; } = string.Empty;
-        public string DatabaseName { get; set; } = string.Empty;
-    }
-}
--- DTO/Publisher.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameStore.Models.DTO
-{
-    public class Publisher
-    {
-        public string Id { get; set; }
-
-        public string Name { get; set; }
-    }
-}
